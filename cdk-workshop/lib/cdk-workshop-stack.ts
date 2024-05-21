c0376b236430b360225354f7afad594f1ff616f1
mxcheung <mxcheung@users.noreply.github.com>
2024-05-21 17:52:19 +1000

    Update cdk-workshop-stack.ts

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -15,7 +15,7 @@ export class CdkWorkshopStack extends Stack {
 
     // defines an AWS Lambda resource
     const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
+      runtime: lambda.Runtime.NODEJS_20_X,    // execution environment
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
       handler: 'hello.handler'                // file is "hello", function is "handler"
     });
