de4a758d2f3622d0e571898587c7b716f4ea7f35
mxcheung <mxcheung@users.noreply.github.com>
2024-05-21 18:01:06 +1000

    Create pipeline-stack.ts

---
+++ cdk-workshop/lib/pipeline-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from 'aws-cdk-lib';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import { Construct } from 'constructs';
+
+export class WorkshopPipelineStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // Pipeline code goes here
+        
+        // Creates a CodeCommit repository called 'WorkshopRepo'
+        new codecommit.Repository(this, 'WorkshopRepo', {
+            repositoryName: "WorkshopRepo"
+        });
+
+        // Pipeline code goes here
+
+    }
+}
