1bb90b0c1462cd56d567b00d83a7e356b9df2e8e
mxcheung <mxcheung@users.noreply.github.com>
2024-05-21 17:20:21 +1000

    Create install.sh

---
+++ cdk-workshop/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Creates pipeline via cdk
+
+echo "Welcome fortunes pipeline"
+
+cd /home/ec2-user/environment/fortune-cookies/pipeline-cdk
+
+cdk init app --language typescript
+
+npm install @aws-cdk/aws-dynamodb @aws-cdk/aws-lambda @aws-cdk/aws-apigateway @aws-cdk/core aws-sdk @aws-cdk/aws-iam
+
+npm install @aws-cdk/aws-codebuild @aws-cdk/aws-codecommit @aws-cdk/aws-codedeploy @aws-cdk/aws-codepipeline @aws-cdk/aws-codepipeline-actions @aws-cdk/pipelines
+
+npm install @aws-cdk/aws-s3
+
+
+cdk bootstrap
+
+cdk synth
+
+cdk deploy
+
