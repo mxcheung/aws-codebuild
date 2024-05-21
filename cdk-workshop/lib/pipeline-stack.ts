74a92c76a2529105bb0ba4373f4e296407bbc335
mxcheung <mxcheung@users.noreply.github.com>
2024-05-21 18:30:02 +1000

    Update pipeline-stack.ts

--- cdk-workshop/lib/pipeline-stack.ts
+++ cdk-workshop/lib/pipeline-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as codecommit from 'aws-cdk-lib/aws-codecommit';
 import { Construct } from 'constructs';
+import {CodeBuildStep, CodePipeline, CodePipelineSource} from "aws-cdk-lib/pipelines";
 
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,12 +9,32 @@ export class WorkshopPipelineStack extends cdk.Stack {
 
         // Pipeline code goes here
         
-        // Creates a CodeCommit repository called 'WorkshopRepo'
-        new codecommit.Repository(this, 'WorkshopRepo', {
+        // This creates a new CodeCommit repository called 'WorkshopRepo'
+        const repo = new codecommit.Repository(this, 'WorkshopRepo', {
             repositoryName: "WorkshopRepo"
         });
-
         // Pipeline code goes here
+
+
+
+        // The basic pipeline declaration. This sets the initial structure
+        // of our pipeline
+       const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'WorkshopPipeline',
+            synth: new CodeBuildStep('SynthStep', {
+                    input: CodePipelineSource.codeCommit(repo, 'main'),
+                    installCommands: [
+                        'npm install -g aws-cdk'
+                    ],
+                    commands: [
+                        'npm ci',
+                        'npm run build',
+                        'npx cdk synth'
+                    ]
+                }
+            )
+        });
+
 
     }
 }
