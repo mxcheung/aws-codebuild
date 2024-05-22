a4df600b76c1f8f07e668daee37b25de0745dd23
mxcheung <mxcheung@users.noreply.github.com>
2024-05-23 08:56:19 +1000

    Create install.sh

---
+++ multi-repo/cdk-pipelines/install.sh
@@ -0,0 +1,5 @@
+mkdir cdk-pipelines
+cd cdk-pipelines
+cdk init app --language typescript
+
+npm install @aws-cdk/aws-codepipeline @aws-cdk/aws-codepipeline-actions @aws-cdk/aws-codecommit @aws-cdk/aws-codebuild
