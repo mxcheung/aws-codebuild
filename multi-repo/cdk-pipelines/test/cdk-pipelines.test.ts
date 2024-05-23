77bd7822f5eede3d7b823302d503a7ba2f2b0eab
max <maxcheung@optusnet.com.au>
2024-05-23 23:19:47 +1000

    init commit cdk workshop

---
+++ multi-repo/cdk-pipelines/test/cdk-pipelines.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPipelines from '../lib/cdk-pipelines-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pipelines-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPipelines.CdkPipelinesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
