a7282cc085a769995f0e3ffbddb6ed50057bd6c3
mxcheung <mxcheung@users.noreply.github.com>
2024-05-23 22:53:48 +1000

    Update install.sh

--- multi-repo/cdk-pipelines/install.sh
+++ multi-repo/cdk-pipelines/install.sh
@@ -1,5 +1,22 @@
-mkdir cdk-pipelines
-cd cdk-pipelines
+#!/bin/bash
+# Creates pipeline via cdk
+
+echo "Welcome fortunes cdk workshop multi repo pipeline"
+cd /home/ec2-user/environment/aws-codebuild/multi-repo/cdk-pipelines
+
 cdk init app --language typescript
 
-npm install @aws-cdk/aws-codepipeline @aws-cdk/aws-codepipeline-actions @aws-cdk/aws-codecommit @aws-cdk/aws-codebuild
+npm install @aws-cdk/aws-dynamodb @aws-cdk/aws-lambda @aws-cdk/aws-apigateway @aws-cdk/core aws-sdk @aws-cdk/aws-iam
+
+npm install @aws-cdk/aws-codebuild @aws-cdk/aws-codecommit @aws-cdk/aws-codedeploy @aws-cdk/aws-codepipeline @aws-cdk/aws-codepipeline-actions @aws-cdk/pipelines
+
+npm install @aws-cdk/aws-s3
+
+
+cdk bootstrap
+
+cdk synth
+
+cdk deploy
+
+
