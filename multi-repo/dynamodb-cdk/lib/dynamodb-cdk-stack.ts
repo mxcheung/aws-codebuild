9336a6e062a1c5b6df9bed7fbebc46c34d54655f
mxcheung <mxcheung@users.noreply.github.com>
2024-06-01 14:14:11 +1000

    Update dynamodb-cdk-stack.ts

--- multi-repo/dynamodb-cdk/lib/dynamodb-cdk-stack.ts
+++ multi-repo/dynamodb-cdk/lib/dynamodb-cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { RemovalPolicy, Stack, StackProps, Duration } from 'aws-cdk-lib';
 
 export class DynamodbCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,6 +20,16 @@ export class DynamodbCdkStack extends cdk.Stack {
     this.createDynamoDBTable('CookiesTable1');
     this.createDynamoDBTable('CookiesTable2');
     this.createDynamoDBTable('CookiesTable3');
+
+     //define dynamodb table
+    const table = new dynamodb.Table(this, id, {
+      partitionKey: { name: "fort_id", type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: RemovalPolicy.DESTROY,
+      tableName: "fortunes"
+      }
+    )
+
+
   }
 
   private createDynamoDBTable(tableName: string) {
