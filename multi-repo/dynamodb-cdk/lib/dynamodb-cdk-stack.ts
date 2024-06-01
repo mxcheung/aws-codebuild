d547e2c83943fee5aeb7cb2e03a3674bf619fa3a
max <maxcheung@optusnet.com.au>
2024-06-01 10:30:50 +1000

    add dynamodb-cdk

--- multi-repo/dynamodb-cdk/lib/dynamodb-cdk-stack.ts
+++ multi-repo/dynamodb-cdk/lib/dynamodb-cdk-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class DynamodbCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,18 +14,8 @@ export class DynamodbCdkStack extends cdk.Stack {
     // });
 
 
-    // Create multiple DynamoDB tables using the helper function
-    this.createDynamoDBTable('CookiesTable1');
-    this.createDynamoDBTable('CookiesTable2');
-    this.createDynamoDBTable('CookiesTable3');
-  }
-  
-  private createDynamoDBTable(tableName: string) {
-    new dynamodb.Table(this, tableName, {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'cookieType', type: dynamodb.AttributeType.STRING },
-      tableName: tableName
-    });
+
+
 
   }
 }
