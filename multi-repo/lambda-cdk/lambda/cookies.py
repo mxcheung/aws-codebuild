ac9ca247f6624bfc450129dad9aae49c62faeab1
mxcheung <mxcheung@users.noreply.github.com>
2024-07-21 09:58:23 +1000

    Update cookies.py

--- multi-repo/lambda-cdk/lambda/cookies.py
+++ multi-repo/lambda-cdk/lambda/cookies.py
@@ -20,8 +20,26 @@ logger.setLevel(logging.INFO)
 
 @xray_recorder.capture('get_fortune_id')
 def get_fortune_id():
+     print("get_fortune_id")
      fortid = (random.randint(1,16))
      return fortid
+
+@xray_recorder.capture('get_fortune')
+def get_fortune():
+    dynamodb = boto3.resource("dynamodb")
+    fortid = get_fortune_id()
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table('fortunes')
+    response = table.get_item(
+        Key={
+            'fort_id': fortid
+        },
+        ProjectionExpression='fortune'
+    )
+    json_string = json.dumps(response)
+    resp_dict=json.loads(json_string)
+    fort_string=json.dumps(resp_dict['Item'])
+    return fort_string
 
 
 @xray_recorder.capture("cookieshandler")
@@ -30,24 +48,9 @@ def lambda_handler(event, context):
     from boto3.dynamodb.conditions import Key, Attr
     from botocore.vendored import requests
     logger.info('got event{}'.format(event))
-    print("get_fortune_id")
-    fortid = get_fortune_id()
-    dynamodb = boto3.resource("dynamodb")
-    xray_recorder.begin_subsegment("get_fortune")
-    table = dynamodb.Table('fortunes')
-
-    response = table.get_item(
-        Key={
-            'fort_id': fortid
-        },
-        ProjectionExpression='fortune'
-        
-    )
-    xray_recorder.end_subsegment()
-    
-    json_string = json.dumps(response)
-    resp_dict=json.loads(json_string)
-    fort_string=json.dumps(resp_dict['Item'])
+#    fortid = get_fortune_id()
+#    dynamodb = boto3.resource("dynamodb")
+    fort_string = get_fortune()
     fort_dict=json.loads(fort_string)
 
     resp = {
