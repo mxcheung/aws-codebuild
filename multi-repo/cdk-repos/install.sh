4a70414404c2313f18ef165803461da3c5081629
mxcheung <mxcheung@users.noreply.github.com>
2024-07-28 17:18:49 +1000

    Update install.sh

--- multi-repo/cdk-repos/install.sh
+++ multi-repo/cdk-repos/install.sh
@@ -22,41 +22,41 @@ cdk deploy
 echo "git clone repos"
 
 cd /home/ec2-user/environment/aws-codebuild/multi-repo/
-git clone codecommit::us-east-1://repo1
-git clone codecommit::us-east-1://repo2
-git clone codecommit::us-east-1://repo3
+git clone codecommit::us-east-1://api_gateway_repo
+git clone codecommit::us-east-1://dynamodb_repo
+git clone codecommit::us-east-1://lambda_repo
 
 echo "git commit code to repos"
 
-echo "git commit dynamodb-cdk to repo1"
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/bin  /home/ec2-user/environment/aws-codebuild/multi-repo/repo1
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/lambda  /home/ec2-user/environment/aws-codebuild/multi-repo/repo1
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/lib  /home/ec2-user/environment/aws-codebuild/multi-repo/repo1
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/test  /home/ec2-user/environment/aws-codebuild/multi-repo/repo1
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/*  /home/ec2-user/environment/aws-codebuild/multi-repo/repo1
-cd /home/ec2-user/environment/aws-codebuild/multi-repo/repo1
+echo "git commit dynamodb-cdk to dynamodb_repo"
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/bin  /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/lambda  /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/lib  /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/test  /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb-cdk/*  /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb_repo
+cd /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb_repo
 git add .
 git commit -m "initial commit"
 git push origin master
 
-echo "git commit lambda-cdk to repo2"
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/bin  /home/ec2-user/environment/aws-codebuild/multi-repo/repo2
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/lambda  /home/ec2-user/environment/aws-codebuild/multi-repo/repo2
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/lib  /home/ec2-user/environment/aws-codebuild/multi-repo/repo2
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/test  /home/ec2-user/environment/aws-codebuild/multi-repo/repo2
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/*  /home/ec2-user/environment/aws-codebuild/multi-repo/repo2
-cd /home/ec2-user/environment/aws-codebuild/multi-repo/repo2
+echo "git commit lambda-cdk to lambda_repo"
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/bin  /home/ec2-user/environment/aws-codebuild/multi-repo/lambda_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/lambda  /home/ec2-user/environment/aws-codebuild/multi-repo/lambda_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/lib  /home/ec2-user/environment/aws-codebuild/multi-repo/lambda_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/test  /home/ec2-user/environment/aws-codebuild/multi-repo/lambda_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/lambda-cdk/*  /home/ec2-user/environment/aws-codebuild/multi-repo/lambda_repo
+cd /home/ec2-user/environment/aws-codebuild/multi-repo/lambda_repo
 git add .
 git commit -m "initial commit"
 git push origin master
 
-echo "git commit api-gateway-cdk to repo3"
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/bin  /home/ec2-user/environment/aws-codebuild/multi-repo/repo3
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/lambda  /home/ec2-user/environment/aws-codebuild/multi-repo/repo3
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/lib  /home/ec2-user/environment/aws-codebuild/multi-repo/repo3
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/test  /home/ec2-user/environment/aws-codebuild/multi-repo/repo3
-cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/*  /home/ec2-user/environment/aws-codebuild/multi-repo/repo3
-cd /home/ec2-user/environment/aws-codebuild/multi-repo/repo3
+echo "git commit api-gateway-cdk to api_gateway_repo"
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/bin  /home/ec2-user/environment/aws-codebuild/multi-repo/api_gateway_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/lambda  /home/ec2-user/environment/aws-codebuild/multi-repo/api_gateway_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/lib  /home/ec2-user/environment/aws-codebuild/multi-repo/api_gateway_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/test  /home/ec2-user/environment/aws-codebuild/multi-repo/api_gateway_repo
+cp -Rf /home/ec2-user/environment/aws-codebuild/multi-repo/api-gateway-cdk/*  /home/ec2-user/environment/aws-codebuild/multi-repo/api_gateway_repo
+cd /home/ec2-user/environment/aws-codebuild/multi-repo/api_gateway_repo
 git add .
 git commit -m "initial commit"
 git push origin master
