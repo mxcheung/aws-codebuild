a649fdd672311ec9a40a4347a2f3b082fe1d1d36
mxcheung <mxcheung@users.noreply.github.com>
2024-08-05 17:20:12 +1000

    Update setup.sh

--- multi-repo/setup.sh
+++ multi-repo/setup.sh
@@ -21,6 +21,16 @@ cd /home/ec2-user/environment/aws-codebuild/multi-repo/cdk-pipelines
 . ./retry_stage.sh api_gateway_repo-Pipeline  Deploy
 cd /home/ec2-user/environment/aws-codebuild/multi-repo/dynamodb_repo
 aws dynamodb batch-write-item --request-items file://items.json
+
+# Number of minutes to loop
+DURATION=3
+# Loop for the specified duration
+for (( i=1; i<=DURATION; i++ ))
+do
+  echo "Minute $i: Status message"
+  sleep 60
+done
+
 cd /home/ec2-user/environment/aws-codebuild/multi-repo/s3_repo
 . ./replace-api-id.sh  
 . ./make-bucket-public.sh
