58b8ed77861d2759ccd106b68f663feca772c6b4
mxcheung <mxcheung@users.noreply.github.com>
2024-08-10 07:59:48 +1000

    Update env_setup.sh

--- multi-repo/cdk-pipelines/env_setup.sh
+++ multi-repo/cdk-pipelines/env_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Variables
-REPLACE_REGION="{REPLACE_ACCOUNT_ID}"
+REPLACE_REGION="{REPLACE_REGION}"
 REPLACE_ACCOUNT_ID="{REPLACE_ACCOUNT_ID}"
 CDK_PIPELINE_FILE="bin/cdk-pipelines.ts"
 
@@ -10,3 +10,6 @@ CURRENT_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
 sed -i "s/${REPLACE_REGION}/${CURRENT_REGION}/g" "$CDK_PIPELINE_FILE"
 sed -i "s/${REPLACE_ACCOUNT_ID}/${CURRENT_ACCOUNT_ID}/g" "$CDK_PIPELINE_FILE"
+
+echo "Replaced ${REPLACE_REGION} with ${CURRENT_REGION} in ${CDK_PIPELINE_FILE}"
+echo "Replaced ${REPLACE_ACCOUNT_ID} with ${CURRENT_ACCOUNT_ID} in ${CDK_PIPELINE_FILE}"
